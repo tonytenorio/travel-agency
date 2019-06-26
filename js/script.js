83e8a05b3881595706813fbae369ea3bdc4e1b30
tony <tonytenorioc@gmail.com>
2019-06-25 22:09:28 -0600

    Slider and Menu Section completed

---
+++ js/script.js
@@ -0,0 +1,64 @@
+
+/*=============================================
+BANNER
+=============================================*/
+
+$(".fade-slider").jdSlider({
+
+	isSliding: false,
+	isAuto: true,
+	isLoop: true,
+	isDrag: false,
+	interval:5000,
+	isCursor: false,
+	speed:3000
+
+});
+
+var alturaBanner = $(".fade-slider").height();
+
+$(".bannerEstatico").css({"height":alturaBanner+"px"})
+
+/*=============================================
+ANIMACIONES SCROLL
+=============================================*/
+
+$(window).scroll(function(){
+
+	var posY = window.pageYOffset;
+	
+	if(posY > alturaBanner){
+
+		$("header").css({"background":"white"})
+
+		$("header .logotipo").css({"filter":"invert(100%)"})
+
+		$(".fa-search, .fa-bars").css({"color":"black"})
+
+	}else{
+
+		$("header").css({"background":"rgba(0,0,0,.5)"})
+
+		$("header .logotipo").css({"filter":"invert(0%)"})
+
+		$(".fa-search, .fa-bars").css({"color":"white"})
+
+	}
+
+})
+
+/*=============================================
+MENÚ
+=============================================*/
+
+$(".fa-bars").click(function(){
+
+	$(".menu").fadeIn("slow");
+
+})
+
+$(".btnClose").click(function(){
+
+	$(".menu").fadeOut("slow");
+
+})
